9729958c44456d99b10015df5fa111323a28fa13
zakie071 <emmanuelprah51@gmail.com>
2023-12-21 09:49:34 +0000

    invoice form crud

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,6 +1,10 @@
 'use server';
 
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -9,12 +13,50 @@ const FormSchema = z.object({
   date: z.date(),
 });
 
-const createInvoice = FormSchema.omit({ id: true, date: true });
-export async function CreateInvoice(formData: FormData) {
-  const { customerId, amount, status } = createInvoice.parse({
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  
+  const amountInCents = amount * 100
+  const date = new Date().toISOString().split('T')[0]
+  // console.log('log');
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,11 +7,11 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { CreateInvoice } from '@/app/lib/action';
+import { createInvoice } from '@/app/lib/action';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form action={CreateInvoice}>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
